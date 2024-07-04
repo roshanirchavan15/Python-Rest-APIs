d8a10cf300fe6640d21a3a60bbb5b002edfee6c8
Roshani Chavan <roshanirchavan15@gmail.com>
2024-07-04 11:26:08 -0700

    Resolving flake8 errors

--- myapi/clean_phone_numbers.py
+++ myapi/clean_phone_numbers.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from mysql.connector import connect, Error
 from config import MYSQL_CREDS
 
-#Function to get the phone numbers
+
+# Function to get the phone numbers
 def parse_phone_number(phone):
     if phone:
         # Remove any non-numeric characters
@@ -14,8 +15,13 @@ def parse_phone_number(phone):
             return '1' + phone
     return None
 
-#opening the directory to get the SQL query to get the faculty id, phone
-with open("myapi/queries/distinguished_professors.sql", "r", encoding="utf8") as mysql_file:
+
+# opening the directory to get the SQL query to get the faculty id, phone
+with open(
+    "myapi/queries/distinguished_professors.sql",
+    "r",
+    encoding="utf8"
+) as mysql_file:
     mysql_query = mysql_file.read()
 
 try:
@@ -29,21 +35,23 @@ try:
     ) as connection:
         with connection.cursor() as cursor:
             cursor.execute(mysql_query)
-            mysql_result = cursor.fetchall() #fetching the result
+            mysql_result = cursor.fetchall()  # fetching the result
 except Error as e:
 
     print(f"failed to connect to mysql db: {e}")
 
-#List to later convert into CSV. List will store faculty_id, phone
-cleaned_data=[]
+# List to later convert into CSV. List will store faculty_id, phone
+cleaned_data = []
 for x in mysql_result:
-    faculty_id, phone = x[0], x[1] #splitting the tuple
-    parse_phone=parse_phone_number(phone)
+    faculty_id, phone = x[0], x[1]  # splitting the tuple
+    parse_phone = parse_phone_number(phone)
     if parse_phone:
         cleaned_data.append([faculty_id, parse_phone])
 
-timestamp = datetime.now(pytz.timezone('America/Los_Angeles')).strftime('%Y%m%d_%H%M%S')
-filename = "myapi/data/clean_phone_numbers_" + timestamp +".csv"
+timestamp = datetime.now(
+    pytz.timezone('America/Los_Angeles')
+    ).strftime('%Y%m%d_%H%M%S')
+filename = "myapi/data/clean_phone_numbers_" + timestamp + ".csv"
 
 with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter='|')
